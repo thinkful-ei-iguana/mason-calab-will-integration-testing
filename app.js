d30f8518546f68a7a4407e87ee23cac774b8f179
Mason Reichbauer <mreichba@mail.usf.edu>
2019-12-03 12:45:34 -0500

    done with assign 1

--- app.js
+++ app.js
@@ -1,10 +1,47 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const playstore = require('./playstore/playstore');
 
 app.use(morgan('dev'));
 app.get('/apps', (req, res) => {
-  // ALL OUR CODE HERE
+  const {sort, genres} = req.query;
+  let results= playstore;
+
+  if(sort) {
+    if (!['Rating', 'App'].includes(sort)) {
+      return res.status(400).send('sort must contain "rating" or "app"');
+    }
+  }
+
+  if (sort==='App') {
+    results=
+    results.sort((a, b) => {
+      return a['App'] < b['App'] ? -1 : 1;
+    });
+  }
+
+  if (sort==='Rating') {
+    results=
+    results.sort((a, b) => {
+      return a['Rating'] > b['Rating'] ? -1 : 1;
+    });
+  }
+
+  const validGenres = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'];
+  if(genres){
+    if (!validGenres.includes(genres)) {
+      return res.status(400).send('genres must be either "Action", "Puzzle", "Strategy", "Casual", "Arcade", "Card"');
+    }
+    
+  }
+
+  if (genres) {
+    results = 
+    results.filter(app => app.Genres.includes(genres));
+  }
+
+  res.json(results);
 });
 
 app.listen(8000, () => {
