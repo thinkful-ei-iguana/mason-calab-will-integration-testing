46474c555eba53ee42dc3ad3e572deb02ebbd8ee
stephenschab <sschab689@gmail.com>
2019-12-04 10:28:50 -0700

    finished should return an array of apps test

--- test/app.test.js
+++ test/app.test.js
@@ -3,9 +3,19 @@ const supertest = require('supertest');
 const app = require('../app');
 
 describe('Express App', () => {
-  it('should return a message from GET /', () => {
+  it('should return an array of apps', () => {
     return supertest(app)
       .get('/apps')
-      .expect(200);
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf.at.least(1);
+        const app = res.body[0];
+        expect(app).to.include.keys(
+          'Category', 'Rating', 'Reviews', 'Installs'
+        );
+      });
   });
+
 });
